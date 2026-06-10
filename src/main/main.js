02bcd75bdf6b7943a33e71c8a477c4fd384f18d5
spellobot <cookieeater699@gmail.com>
2026-06-10 18:24:47 +0100

    feat: update essential files to match recommended structure and content

---
+++ src/main/main.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2026 Ari Brandão
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { join } = require('path');
+const os = require('os');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: join(__dirname, '../preload/preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    });
+    win.loadFile(join(__dirname, '../renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+
+    app.on('window-all-closed', () => {
+        if (process.platform != 'darwin') app.quit();
+    });
+});
